2856f70030f194b4ce715e80a6b8f4824c2e8773
mattshirtliffe <matt.shirtliffe@gmail.com>
2018-04-13 18:07:26 +0100

    post and get

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # Run
 
 pipenv --python 3.6
+
 pipenv shell
 
 pipenv install flask
+
 pipenv install riak
 
 export FLASK_APP=main.py
+
+export FLASK_DEBUG=1
+
 flask run
--- main.py
+++ main.py
@@ -1,58 +1,53 @@
 from riak import RiakClient, RiakNode
 from flask import Flask, request, jsonify, url_for
 
-
 app = Flask(__name__)
-# client = RiakClient()
-# create a bucket
-# myBucket = client.bucket('test')
+client = RiakClient()
+
+# create and get bucket
+bucket = client.bucket('test')
 
 @app.route("/")
 def index():
-    return jsonify({'message':'All the actions starts at '+ request.url
-    + url_for('riak_route')[1:]})
+    return jsonify({'message': 'All the actions starts at ' + request.url
+                               + url_for('riak_route')[1:]})
 
-@app.route("/riak", defaults={'key': None}, methods=['GET','POST'])
+
+@app.route("/riak", defaults={'key': None}, methods=['GET', 'POST'])
 @app.route("/riak/<key>", methods=['GET'])
 def riak_route(key):
     if request.method == 'POST':
-        pass
-    elif request.method == 'DELETE':
-        pass
-    else:
-        return "Hello World!"
 
-# #  {'one':1}
-# val1 = 1
-# key1 = myBucket.new('one', data=val1)
-# key1.store()
-#
-#
-# val2 = "two"
-# key2 = myBucket.new('two', data=val2)
-# key2.store()
-#
-#
-# val3 = {"myValue": 3}
-# key3 = myBucket.new('three', data=val3)
-# key3.store()
-#
-#
-# fetched1 = myBucket.get('one')
-# fetched2 = myBucket.get('two')
-# fetched3 = myBucket.get('three')
-#
-#
-# print(val1 == fetched1.data)
-# print(val2 == fetched2.data)
-# print(val3 == fetched3.data)
-#
-#
-# fetched3.data['myValue'] = 4
-# fetched3.store()
-#
-# print(fetched3.data)
-# fetched3.delete()
-# print(fetched3)
-#
-# print(myBucket.get('three').exists)
+        # get key and value from request
+        # if request.json:
+        new_key = request.json['key']
+        data = request.json['data']
+
+        # check both key and data are set - truthy
+        if new_key and data:
+            new = bucket.new(new_key, data=data)
+            new.store()
+            if bucket.get(new_key).exists:
+                fetched = bucket.get(new_key)
+                return jsonify(fetched.data)
+        else:
+            return jsonify({'message':'missing key or data'}), 404
+
+    elif request.method == 'DELETE':
+
+        if bucket.get(key).exists:
+            fetched = bucket.get(key)
+            fetched.delete()
+            return jsonify(fetched)
+        else:
+            return jsonify({'message': key + ' not found'}), 404
+    else:
+        if not key:
+            return jsonify({'message': 'give me a key I am hungry '+request.url
+                               +'/<key>'}), 404
+        else:
+            if bucket.get(key).exists:
+                fetched = bucket.get(key)
+                return jsonify(fetched.data)
+            else:
+                return jsonify({'message': key + ' not found'}), 404
